13e6afc2b7979c950f57d13b75e78febb53b613d
030-cpu <emiliano101805@icloud.com>
2026-04-29 14:26:16 -0700

    Set up bakery game structure and customer details
    Initialize bakery game with customer data and game functions.

---
+++ bakery.js
@@ -0,0 +1,75 @@
+// basic variables
+let gameStart = false;
+let customerLevel = 1;
+let money = 50;
+let tips = 0;
+
+// customers level 1
+let customer1 = {
+    name: " Hailey ", 
+    order: "candy",
+};
+
+let customers2 = {
+    name: " Audrina ", 
+    order: "candy, lolipop",
+}
+
+let customers3 = {
+    name: " Rylan ", 
+    order: "candy, candy, lolipop",
+}
+
+let customer4 = {
+    name: " Nicholas ", 
+    order: "candy, milk chocolate ",
+}
+
+let customers5 = {
+    name: " Eileen ", 
+    order: "lolipop, lolipop, milk chocolate",
+}
+
+let customers6 = {
+    name: " Roger ", 
+    order: "candy, milk chocolate, milk chooclate",
+}
+
+// Start Game Functions
+    function playGame(){
+        let gameStart = true;
+        
+    }; 
+
+    function stopGame(){
+        let gameStart = false;
+
+    }; 
+
+// Customers Functions
+
+// Navigation Functions 
+
+// Treats Functions 
+
+// Ingredients Functions
+
+/* 
+let allFoodOptions = [
+
+]
+
+setinterval 100000{fucntion()}
+
+function(){
+    customerOrder = []
+    for(i=0; i<7; i++)
+    item = math.radom allfoodoptions
+
+    let customer1 = new createCustomer("foidrina", customerOrder)
+}
+
+class createCustomer {
+....
+}
+*/
